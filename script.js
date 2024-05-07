2fa80c3e57f239afbc291dcc14b5cbe963a3e996
Dobre Tiberiu-Gabriel <127014075+Tibisorul@users.noreply.github.com>
2024-05-07 13:12:09 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById('github1').addEventListener('click', function() {
+    window.open('https://github.com/tibisorul', '_blank');
+});
+
+document.getElementById('github2').addEventListener('click', function(){
+    window.open('https://github.com/Tibisorul/Database', '_blank');
+})
+
+document.getElementById('github3').addEventListener('click', function(){
+    window.open('https://github.com/Tibisorul/MyCV-ToPractice','_blank');
+})
