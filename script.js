5755d58a833945381b00354284b58f2bb7642da8
Dobre Tiberiu-Gabriel <127014075+Tibisorul@users.noreply.github.com>
2024-05-07 21:12:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,34 @@
 document.getElementById('github1').addEventListener('click', function() {
+    alert("Please confirm that you want to proceed to the next page.");
     window.open('https://github.com/tibisorul', '_blank');
 });
 
 document.getElementById('github2').addEventListener('click', function(){
+    alert("Please confirm that you want to proceed to the next page.");
     window.open('https://github.com/Tibisorul/Database', '_blank');
 })
 
 document.getElementById('github3').addEventListener('click', function(){
+    alert("Please confirm that you want to proceed to the next page.");
     window.open('https://github.com/Tibisorul/MyCV-ToPractice','_blank');
 })
+
+document.addEventListener('DOMContentLoaded', function() {
+    const buttons = document.querySelectorAll('.show-popup');
+    const closeButtons = document.querySelectorAll('.close-btn');
+
+    buttons.forEach((button) => {
+        button.addEventListener('click', function() {
+            const popup = button.closest('.card').querySelector('.popup-container');
+            popup.classList.add('active');
+        });
+    });
+
+    closeButtons.forEach((button) => {
+        button.addEventListener('click', function() {
+            const popup = button.closest('.popup-container');
+            popup.classList.remove('active');
+        });
+    });
+});
+
